5307fc2234f80beb0e383a7234f0d7032363e42d
Jörg Schönberger <office@webchills.at>
2023-12-13 19:33:21 +0100

    remove unused config settings for carrier 6

--- NEUE DATEIEN/DEINADMIN/includes/installers/paket-tracking/2_6_0.php
+++ NEUE DATEIEN/DEINADMIN/includes/installers/paket-tracking/2_6_0.php
@@ -4,7 +4,7 @@
  * @copyright Copyright 2003-2022 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: 2_6_0.php  2023-12-13 12:46:51Z webchills $
+ * @version $Id: 2_6_0.php  2023-12-13 19:19:51Z webchills $
  */
  
 $db->Execute(" SELECT @gid:=configuration_group_id
@@ -31,7 +31,7 @@ $db->Execute("INSERT IGNORE INTO ".TABLE_CONFIGURATION." (configuration_title, c
 ('Package Tracking - Carrier 5 Tracking Link', 'CARRIER_LINK_5', 'https://www.post.at/sendungsverfolgung.php/details?pnum1=', 'Enter the tracking link of Carrier 5<br /> <br /><strong>Example:</strong> http://www.post.at/tnt_query.php?pnum1=', @gid, 15, now(), now(), NULL, NULL),
 ('Package Tracking - Carrier 6 Status', 'CARRIER_STATUS_6', 'False', 'Enable Tracking for Carrier 6<br /><br />Set to false if you do NOT want Carrier 6 to be displayed on Admin and Customer page.', @gid, 16, now(), now(), NULL, 'zen_cfg_select_option(array(\'True\', \'False\'),'),
 ('Package Tracking - Carrier 6 Name', 'CARRIER_NAME_6', 'Deutsche Post', 'Enter name of Carrier 6 <br /> <br /><strong>Example:</strong> Deutsche Post<br />(default: blank)', @gid, 17, now(), now(), NULL, NULL),
-('Package Tracking - Carrier 6 Tracking Link', 'CARRIER_LINK_6', 'https://www.deutschepost.de/de/s/sendungsverfolgung.html?piececode=', 'Enter the tracking link of Carrier 6<br /> <br /><strong>Example:</strong> https://www.deutschepost.de/sendung/simpleQueryResult.html?form.sendungsnummer=', @gid, 18, now(), now(), NULL, NULL),
+('Package Tracking - Carrier 6 Tracking Link', 'CARRIER_LINK_6', 'https://www.deutschepost.de/de/s/sendungsverfolgung.html?piececode=', 'Enter the tracking link of Carrier 6<br /> <br /><strong>Example:</strong> https://www.deutschepost.de/de/s/sendungsverfolgung.html?piececode=', @gid, 18, now(), now(), NULL, NULL),
 ('Package Tracking - Max display for Track Order sidebox', 'MAX_DISPLAY_PRODUCTS_IN_TRACK_ORDERS_BOX', '3', 'The maximum number of orders to display on the Track Order sidebox ', @gid, 22, now(), now(), NULL, NULL)");                             
 
 
@@ -52,7 +52,7 @@ $db->Execute("REPLACE INTO ".TABLE_CONFIGURATION_LANGUAGE." (configuration_title
 ('Paket Tracking - Versandunternehmen 5 Tracking Link', 'CARRIER_LINK_5','Geben Sie den Tracking Link von Versandunternehmen 5 ein.<br /> Beispiel: http://www.post.at/sendungsverfolgung.php/details?pnum1=', 43),
 ('Paket Tracking - Versandunternehmen 5 Status', 'CARRIER_STATUS_5','Wollen Sie das Tracking für Versandunternehmen 5 aktivieren?<br />Auf false setzen, wenn Sie NICHT wollen, dass Versandunternehmen 5 auf der Admin- und der Kundenseite erscheint.', 43),
 ('Paket Tracking - Versandunternehmen 6 Name', 'CARRIER_NAME_6', 'Geben Sie den Namen von Versandunternehmen 6 ein <br />Beispiel: Deutsche Post<br />(Voreingestellt: Deutsche Post)', 43),
-('Paket Tracking - Versandunternehmen 6 Tracking Link', 'CARRIER_LINK_6','Geben Sie Tracking Link von Versandunternehmen 6 ein.<br /> Beispiel: https://www.deutschepost.de/sendung/simpleQueryResult.html?form.sendungsnummer=', 43),
+('Paket Tracking - Versandunternehmen 6 Tracking Link', 'CARRIER_LINK_6','Geben Sie Tracking Link von Versandunternehmen 6 ein.<br /> Beispiel: https://www.deutschepost.de/de/s/sendungsverfolgung.html?piececode=', 43),
 ('Paket Tracking - Versandunternehmen 6 Status', 'CARRIER_STATUS_6','Wollen Sie das Tracking für Versandunternehmen 6 aktivieren?<br />Auf false setzen, wenn Sie NICHT wollen, dass Versandunternehmen 6 auf der Admin- und der Kundenseite erscheint.', 43),
 ('Paket Tracking - Sidebox Einstellung', 'MAX_DISPLAY_PRODUCTS_IN_TRACK_ORDERS_BOX','Maximale Anzahl der Bestellungen, die in der Bestellverfolgungs-Sidebox angezeigt werden sollen.', 43)");
 
--- NEUE DATEIEN/DEINADMIN/includes/installers/paket-tracking/3_0_0.php
+++ NEUE DATEIEN/DEINADMIN/includes/installers/paket-tracking/3_0_0.php
@@ -26,4 +26,23 @@
         $sql = "ALTER TABLE ".TABLE_ORDERS_STATUS_HISTORY." DROP COLUMN track_year";
         $db->Execute($sql);
     }
+// delete unused config settings
+$db->Execute("DELETE FROM ".TABLE_CONFIGURATION." WHERE configuration_key LIKE '%CARRIER_LINK_6_PART1%')");  
+$db->Execute("DELETE FROM ".TABLE_CONFIGURATION." WHERE configuration_key LIKE '%CARRIER_LINK_6_PART2%')"); 
+$db->Execute("DELETE FROM ".TABLE_CONFIGURATION." WHERE configuration_key LIKE '%CARRIER_LINK_6_PART3%')"); 
+$db->Execute("DELETE FROM ".TABLE_CONFIGURATION." WHERE configuration_key LIKE '%CARRIER_LINK_6_PART4%')"); 
+$db->Execute("DELETE FROM ".TABLE_CONFIGURATION_LANGUAGE." WHERE configuration_key LIKE '%CARRIER_LINK_6_PART1%')");  
+$db->Execute("DELETE FROM ".TABLE_CONFIGURATION_LANGUAGE." WHERE configuration_key LIKE '%CARRIER_LINK_6_PART2%')"); 
+$db->Execute("DELETE FROM ".TABLE_CONFIGURATION_LANGUAGE." WHERE configuration_key LIKE '%CARRIER_LINK_6_PART3%')"); 
+$db->Execute("DELETE FROM ".TABLE_CONFIGURATION_LANGUAGE." WHERE configuration_key LIKE '%CARRIER_LINK_6_PART4%')"); 
+// insert normal tracking link 6
+$db->Execute(" SELECT @gid:=configuration_group_id
+FROM ".TABLE_CONFIGURATION_GROUP."
+WHERE configuration_group_title= 'Paket Tracking'
+LIMIT 1;");
+$db->Execute("INSERT IGNORE INTO ".TABLE_CONFIGURATION." (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, last_modified, use_function, set_function) VALUES
+('Package Tracking - Carrier 6 Tracking Link', 'CARRIER_LINK_6', 'https://www.deutschepost.de/de/s/sendungsverfolgung.html?piececode=', 'Enter the tracking link of Carrier 6<br /> <br /><strong>Example:</strong> https://www.deutschepost.de/de/s/sendungsverfolgung.html?piececode=', @gid, 18, now(), now(), NULL, NULL)");     
+$db->Execute("REPLACE INTO ".TABLE_CONFIGURATION_LANGUAGE." (configuration_title, configuration_key, configuration_description, configuration_language_id) VALUES
+('Paket Tracking - Versandunternehmen 6 Tracking Link', 'CARRIER_LINK_6','Geben Sie Tracking Link von Versandunternehmen 6 ein.<br /> Beispiel: https://www.deutschepost.de/de/s/sendungsverfolgung.html?piececode=', 43)");
+// set module version to 3.0.0
 $db->Execute("UPDATE " . TABLE_CONFIGURATION . " SET configuration_value = '3.0.0' WHERE configuration_key = 'PAKETTRACKING_MODUL_VERSION' LIMIT 1;");
